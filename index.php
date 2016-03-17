5e643cd0f02654ab09508b45f72d1820db986be4
Jared Ubelhor <jared.ubelhor@outlook.com>
2016-03-16 20:09:58 -0700

    Setup Basic HTML/CSS Template for Pages

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="utf-8">
+    <title>All Star Automotive Point of Sales</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <header>
+        <div style="float: left; margin: auto;">
+            <p>
+                <h1>All Star Auto POS</h1>
+            </p>
+        </div>
+        
+        <div style="position: absolute; bottom: 5px; right: 20px;">
+            <p>
+                Logged in as: <a href="#user_page">FAKE_ADMIN</a> [<a href="#logout">Log Out</a>]
+            </p> 
+        </div>       
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="home.php">🏠 HOME</a></li>
+            <li><a href="#sales">💰 SALES</a></li>
+            <li><a href="#customers">👥 CUSTOMERS</a></li>
+            <li><a href="#vehicles">🚗 VEHICLES</a></li>
+            <li><a href="#reports">💹 REPORTS</a></li>
+            <li><a href="#admin">👮 ADMIN</a></li>
+        </ul>
+    </nav>
+    
+    <section>
+        
+        <div class="info_section">
+            <div class="section_header_alert">
+                <h2>Alert</h2>
+            </div>
+            <div class="section_body_alert">
+                <p>This program is for demonstration purposes only. Do not use in a production environment. Do not use to store real customer information!</p>
+            </div>
+        </div>
+        
+        <br>
+        <br>
+        
+        <div class="info_section">
+            <div class="section_header_error">
+                <h2>ERROR</h2>
+            </div>
+            <div class="section_body_error">
+                <p>This is the error message text.</p>
+            </div>
+        </div>
+                
+        <br>
+        <br>
+        
+        <div class="info_section">
+            <div class="section_header_general">
+                <h2>GENERAL INFORMATION</h2>
+            </div>
+            <div class="section_body_general">
+                <p>This is some informative text about the program.</p>
+            </div>
+        </div>
+        
+    </section>
+</body>    
+    
+</html>
